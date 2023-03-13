48f8769a9bd8b412d78f8f66fcdaf25a2eaa40ea
Walter <walteribanez555@gmail.com>
2023-03-12 22:26:04 -0400

    Template form updated

--- src/app/Modules/polizas/generar-polizas/generar-polizas.component.ts
+++ src/app/Modules/polizas/generar-polizas/generar-polizas.component.ts
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./generar-polizas.component.css']
 })
 export class GenerarPolizasComponent {
+  formStepsNum = 0;
+  progressSteps!: NodeListOf<Element>;
+  formSteps!: NodeListOf<Element>;
+  progress!: HTMLElement;
 
+  formsTitle : string[] = [
+    "Elegir Poliza",
+    "Agregar extensible",
+    "Llenar datos"
+  ]
+
+  formTitle: string = this.formsTitle[0];
+
+ 
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.progressSteps = document?.querySelectorAll(".progress-step");
+    this.formSteps = document.querySelectorAll(".form-step");
+    this.progress = document.getElementById("progress")!;
+    const prevBtns = document.querySelectorAll(".btn-prev");
+    const nextBtns = document.querySelectorAll(".btn-next");
+
+    nextBtns.forEach(btn => {
+      btn.addEventListener("click", ()=> {
+        this.formStepsNum++;
+        this.formTitle = this.formsTitle[this.formStepsNum];
+        this.updateFormSteps();
+        this.updateProgressbar();
+      })
+    })
+
+    prevBtns.forEach(btn => {
+      btn.addEventListener("click", ()=> {
+        this.formStepsNum--;
+        this.updateFormSteps();
+        this.updateProgressbar();
+      })
+    })
+  }
+
+  updateFormSteps(){
+    this.formSteps.forEach(formStep =>  {
+      formStep.classList.contains("form-step-active") &&
+      formStep.classList.remove("form-step-active")
+    })
+    this.formSteps[this.formStepsNum].classList.add("form-step-active")
+  }
+
+  updateProgressbar(){
+    this.progressSteps.forEach((progressStep, idx) => {
+      if(idx < this.formStepsNum + 1){
+        progressStep.classList.add("progress-step-active")
+      } else {
+        progressStep.classList.remove("progress-step-active")
+      }
+    });
+
+    const progressActive = document.querySelectorAll(".progress-step-active");
+    this.progress.style.width = ((progressActive.length - 1) / (this.progressSteps.length - 1)) * 100 + "%";
+  }
 }
