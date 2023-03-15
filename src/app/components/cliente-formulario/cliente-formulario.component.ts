cc3dd164ba0313c4f0c3416f0e2812f83cc47197
Walter <walteribanez555@gmail.com>
2023-03-15 16:14:48 -0400

    Template Updated

---
+++ src/app/components/cliente-formulario/cliente-formulario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-formulario',
+  templateUrl: './cliente-formulario.component.html',
+  styleUrls: ['./cliente-formulario.component.css']
+})
+export class ClienteFormularioComponent {
+
+}
