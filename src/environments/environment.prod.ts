8bb69ef2fc189781e108aa32fd223fd60efa1ee8
walteribanez555 <walteribanez555@gmail.com>
2025-04-01 11:50:28 -0400

    Details updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@
 export const environment = {
   production : true,
   apiUrl: 'https://z1rlqbo0tl.execute-api.us-east-1.amazonaws.com',
-  apiBackend : 'https://5w19y9q7z5.execute-api.us-east-1.amazonaws.com',
+  apiBackend : 'https://nv101ison4.execute-api.us-east-1.amazonaws.com',
   stripe_pk : 'pk_live_51Mz1YlETw1bi8nWN6SIQ1jAtVxlD9MquyQVDHRjtN1nBBGnk30boCCeI6Bh7STzGQcvErsb0Xk6nV3HxdSQ7G5Wz00I5tuLl1r',
   apiNotificationsService : 'https://fbnode-production.up.railway.app/api',
   mapBox_Token : 'pk.eyJ1Ijoid2FsdGVyaWJhbmV6NTU1IiwiYSI6ImNsZThsZWE0aTBnYWMzdm85aDB6ZjdvZGIifQ.SuEw6nfgx1-8t2p_sDZsBg',
